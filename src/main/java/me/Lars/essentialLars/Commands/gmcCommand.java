53e521f08f74654bc035d580c21fbfdaceb58e97
LarsChokier <lars.chokier@student.ehb.be>
2026-02-16 18:55:22 +0100

    Begin gamemode Part

---
+++ src/main/java/me/Lars/essentialLars/Commands/gmcCommand.java
@@ -0,0 +1,76 @@
+package me.Lars.essentialLars.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class gmcCommand implements CommandExecutor {
+
+
+
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+            if (sender instanceof Player){
+                Player player = (Player) sender;
+
+                if (args.length == 0){
+
+                    player.setGameMode(GameMode.CREATIVE);
+                    player.sendMessage(ChatColor.DARK_GRAY + "["
+                            + ChatColor.AQUA + "Server"
+                            + ChatColor.DARK_GRAY + "] "
+                            + ChatColor.GREEN + "Success: "
+                            + ChatColor.GRAY + "Your gamemode has been changed to "
+                            + ChatColor.YELLOW + "creative"
+                            + ChatColor.GRAY + ".");
+
+                }else if (args.length == 1){
+
+                    String playerName = args[0];
+
+                    Player target = Bukkit.getPlayer(playerName);
+
+                    target.setGameMode(GameMode.CREATIVE);
+
+
+                    player.sendMessage(ChatColor.DARK_GRAY + "["
+                            + ChatColor.AQUA + "Server"
+                            + ChatColor.DARK_GRAY + "] "
+                            + ChatColor.GREEN + "Success: "
+                            + ChatColor.GRAY + "You changed "
+                            + ChatColor.YELLOW + target.getName()
+                            + ChatColor.GRAY + "'s gamemode to "
+                            + ChatColor.YELLOW + "creative"
+                            + ChatColor.GRAY + ".");
+
+                    target.sendMessage(ChatColor.DARK_GRAY + "["
+                            + ChatColor.AQUA + "Server"
+                            + ChatColor.DARK_GRAY + "] "
+                            + ChatColor.GREEN + "Success: "
+                            + ChatColor.GRAY + "Your gamemode has been changed to "
+                            + ChatColor.YELLOW + "creative"
+                            + ChatColor.GRAY + " by "
+                            + ChatColor.YELLOW + player.getName()
+                            + ChatColor.GRAY + ".");
+
+                }else {
+
+                    player.sendMessage(ChatColor.DARK_GRAY + "["
+                            + ChatColor.AQUA + "Server"
+                            + ChatColor.DARK_GRAY + "] "
+                            + ChatColor.RED + "Error: "
+                            + ChatColor.GRAY + "Incorrect usage. Use: "
+                            + ChatColor.YELLOW + "/gamemode <mode>");
+
+
+                }
+            }
+            return false;
+        }
+}
--- src/main/java/me/Lars/essentialLars/EssentialLars.java
+++ src/main/java/me/Lars/essentialLars/EssentialLars.java
@@ -2,6 +2,9 @@ package me.Lars.essentialLars;
 
 import me.Lars.essentialLars.Commands.SetSpawnCommand;
 import me.Lars.essentialLars.Commands.SpawnCommand;
+import me.Lars.essentialLars.Commands.gmcCommand;
+import me.Lars.essentialLars.Commands.gmsCommand;
+import me.Lars.essentialLars.Listeners.SpawnListeners;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EssentialLars extends JavaPlugin {
@@ -16,6 +19,10 @@ public final class EssentialLars extends JavaPlugin {
         getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
         getCommand("spawn").setExecutor(new SpawnCommand(this));
 
+        getServer().getPluginManager().registerEvents(new SpawnListeners(this), this);
 
+        //===GAMEMODE COMMANDS===
+        getCommand("gmc").setExecutor(new gmcCommand());
+        getCommand("gms").setExecutor(new gmsCommand());
     }
 }
